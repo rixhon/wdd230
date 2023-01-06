bc8c8a9e35e879d3f914a81504cf61f719ce3ea5
rixhon <rixhon@gmail.com>
2023-01-05 22:54:00 -0300

    second commit

---
+++ 01/scripts/app.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1');
+const copyright = document.querySelector("#copyright");
+
+h1.textContent = 'Michael Franz Rixhon Junior';
+
+copyright.textContent = `© ${new Date().getFullYear()}`
+let quantity = document.querySelector('#q').value;

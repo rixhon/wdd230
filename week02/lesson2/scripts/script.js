d4e1a51b375613746a0e541b1072fc6d4668d7d1
rixhon <rixhon@gmail.com>
2023-01-16 20:53:40 -0300

    design principles upload

---
+++ week02/lesson2/scripts/script.js
@@ -0,0 +1,6 @@
+const copyright = document.querySelector("#copyright");
+copyright.textContent = `© ${new Date().getFullYear()}`
+
+
+lastModified = new Date(document.lastModified);
+document.getElementById('lastupdated').innerHTML = lastModified.toLocaleString();
